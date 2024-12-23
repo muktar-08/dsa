96a6a628af867fe57485ebbd054d67105564c03d
muktar-08 <muktarahmedterdal@gmail.com>
2024-12-23 23:15:13 +0530

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,60 +1,61 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+
 int main()
 {
-char acMainStr[200], acSrchStr[30], acRepStr[30], acResStr[200], acCopyStr[200];
-int i=0, j=0 ,k=0, l, iMtchCnt, iStop, len, iNumOfMatch=0;
-printf("\nEnter the main string :\n");
-scanf(" %[^\n]", acMainStr);
-printf("\nEnter the Pattern string :\n");
-scanf(" %[^\n]", acSrchStr);
-printf("\nEnter the Replace string :\n");
-scanf(" %[^\n]", acRepStr);
-strcpy(acCopyStr, acMainStr);
-for(i=0;i<(strlen(acMainStr)-strlen(acSrchStr)+1);i++)
-{
-iMtchCnt = 0;
-for(j=0;j<strlen(acSrchStr);j++)
-{
-if(acMainStr[i+j] == acSrchStr[j])
-{
-iMtchCnt++;
-}
-else
-{
-break;
-}
-if(iMtchCnt == strlen(acSrchStr)) 
-{
-iNumOfMatch++; 
-for(k=0;k<i;k++)
-{
-acResStr[k] = acMainStr[k]; 
-}
-iStop = k + strlen(acSrchStr); 
-acResStr[k] = '\0';
-strcat(acResStr, acRepStr); 
-len = strlen(acResStr);
-for(k=iStop, l=0; acMainStr[k] != '\0';k++, l++)
+    char st[200], srch[30], rep[30], res[200], cpy[200];
+    int i=0, j=0 ,k=0, l, mtch, iStop, len, nom=0;
+
+    printf("\nEnter the main string\n");
+	scanf(" %[^\n]", st);
+    printf("\nEnter the Pattern string\n");
+	scanf(" %[^\n]", srch);
+    printf("\nEnter the Replace string\n");
+	scanf(" %[^\n]", rep);
+    strcpy(cpy, st);
+    for(i=0;i<(strlen(st)-strlen(srch)+1);i++)
     {
-acResStr[len+l] = acMainStr[k];
-}
-acResStr[len+l] = '\0';
-strcpy(acMainStr,acResStr);
-}
-}
-}
-printf("\nInput Text :\n");
-printf("%s\n",acCopyStr);
-if(iNumOfMatch > 0)
-{
-printf("\n%d matches occured\n\nText after replacing matched patterns is shown below\n", iNumOfMatch);
-printf("\n%s\n",acResStr);
-}
-else
-{
-printf("\nPattern String not found in Text\n");
-}
-return 0;
+        mtch = 0;
+        for(j=0;j<strlen(srch);j++)
+        {
+         if(st[i+j] == srch[j])
+            {
+                mtch++;
+            }
+            else
+            {
+                break;
+            }
+            if(mtch == strlen(srch))
+            {
+                nom++;
+                for(k=0;k<i;k++)
+                {
+                    res[k] = st[k];
+                }
+                iStop = k + strlen(srch);
+                res[k] = '\0';
+                strcat(res, rep);
+                len = strlen(res);
+                for(k=iStop, l=0; st[k] != '\0';k++, l++)
+                {
+                    res[len+l] = st[k];
+                }
+                res[len+l] = '\0';
+                strcpy(st,res);
+            }
+        }
+    }
+    printf("\nInput Text\n");
+    printf("%s\n",cpy);
+    if(nom > 0)
+    {
+        printf("\n%d matches occured\n\nText after replacing matched patterns is shown below\n", nom);
+        printf("\n%s\n",res);
+    }
+    else
+    {
+        printf("\nPattern String not found in Text\n");
+    }
+    return 0;
 }
