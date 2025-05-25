3ad9c31104416cdcc14bc769fbd409218ab58702
muktar-08 <muktarahmedterdal@gmail.com>
2025-05-25 12:11:18 +0530

    lab2.c

--- lab10.c
+++ lab10.c
@@ -1,110 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-struct Node {
-    int data;
-    struct Node *left, *right;
-};
-
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->left = newNode->right = NULL;
-    return newNode;
+#include<stdio.h>
+int cost[10][10],n;
+void prim()
+{
+int vt[10]={0};
+int a=0,b=0,min, mincost = 0, ne = 0;
+vt[0] = 1;
+while(ne < n-1)
+{
+min = 999;
+for (int i = 0; i<n; i++)
+{
+if(vt[i]==1)
+for(int j = 0;j <n; j++)
+if(cost[i][j] < min && vt[j]==0)
+{
+min = cost[i][j];
+a = i;
+b = j;
 }
-
-struct Node* insert(struct Node* root, int data) {
-    if (root == NULL) return createNode(data);
-    if (data < root->data)
-        root->left = insert(root->left, data);
-    else if (data > root->data)
-        root->right = insert(root->right, data);
-    return root;
 }
-
-void inorder(struct Node* root) {
-    if (root) {
-        inorder(root->left);
-        printf("%d ", root->data);
-        inorder(root->right);
-    }
+printf("Edge from vertex %d to vertex %d and the cost %d\n",a,b,min);
+vt[b] = 1;
+ne++;
+mincost += min;
+cost[a][b] = cost[b][a] = 999;
 }
-
-void preorder(struct Node* root) {
-    if (root) {
-        printf("%d ", root->data);
-        preorder(root->left);
-        preorder(root->right);
-    }
+printf("minimum spanning tree cost is %d",mincost);
 }
-
-void postorder(struct Node* root) {
-    if (root) {
-        postorder(root->left);
-        postorder(root->right);
-        printf("%d ", root->data);
-    }
-}
-
-void search(struct Node* root, int key) {
-    if (root == NULL) {
-        printf("Key %d not found.\n", key);
-        return;
-    }
-    if (root->data == key) {
-        printf("Key %d found in the BST.\n", key);
-        return;
-    }
-    if (key < root->data)
-        search(root->left, key);
-    else
-        search(root->right, key);
-}
-
-int main() {
-    struct Node* root = NULL;
-    int choice, key, i;
-    int elements[] = {6, 9, 5, 2, 8, 15, 24, 14, 7, 8, 5, 2};
-    int n = sizeof(elements) / sizeof(elements[0]);
-
-    while (1) {
-        printf("\n--- BST Menu ---\n");
-        printf("1. Create BST\n");
-        printf("2. Traverse BST (Inorder, Preorder, Postorder)\n");
-        printf("3. Search for a key\n");
-        printf("4. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                printf("Creating BST with elements: ");
-                for (i = 0; i < n; i++) {
-                    printf("%d ", elements[i]);
-                    root = insert(root, elements[i]);
-                }
-                printf("\nBST created successfully.\n");
-                break;
-            case 2:
-                printf("Inorder: ");
-                inorder(root);
-                printf("\nPreorder: ");
-                preorder(root);
-                printf("\nPostorder: ");
-                postorder(root);
-                printf("\n");
-                break;
-            case 3:
-                printf("Enter key to search: ");
-                scanf("%d", &key);
-                search(root, key);
-                break;
-            case 4:
-                printf("Exiting program.\n");
-                exit(0);
-            default:
-                printf("Invalid choice. Try again.\n");
-        }
-    }
-    return 0;
+void main()
+{
+printf("Enter the no. of vertices: ");
+scanf("%d",&n);
+printf("Enter the cost matrix\n");
+for(int i=0;i<n;i++)
+for(int j=0;j<n;j++)
+scanf("%d",&cost[i][j]);
+prim();
 }
