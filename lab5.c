ad8dfea7d8f9a1903345df4bd38cc16777748c51
muktar-08 <muktarahmedterdal@gmail.com>
2025-12-01 22:10:44 +0530

    CN lab5.c

--- lab5.c
+++ lab5.c
@@ -1,39 +1,61 @@
-#include<stdio.h>
-int cost[10][10],n,colsum[10];
-void cal_colsum()
+import java.util.*;
+public class Crc
 {
-for(int j=0;j<n;j++)
+void div(int a[],int k)
+{i
+nt gp[]={1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1};
+int count=0;
+for(int i=0;i<k;i++)
+{ if(a[i]==gp[0])
+{ for(int j=i;j<17+i;j++)
+{a
+[j]=a[j]^gp[count++];
+}c
+ount=0;
+}}}
+public static void main(String args[])
+{ int a[]=new int[100];
+int b[]=new int[100];
+int len,k;
+Crc ob=new Crc();
+System.out.println("Enter the length of Data Frame:");
+Scanner sc=new Scanner(System.in);
+len=sc.nextInt();
+int flag=0;
+System.out.println("Enter the Message:");
+for(int i=0;i<len;i++)
+{a
+[i]=sc.nextInt();
+} for(int i=0;i<16;i++)
+{a
+[len++]=0;
+}
+k=len-16;
+for(int i=0;i<len;i++)
 {
-colsum[j]=0;
-for(int i=0;i<n;i++)
-colsum[j]+=cost[i][j];
+b[i]=a[i];
 }
-}
-void source_removal()
+ob.div(a,k);
+for(int i=0;i<len;i++)
+a[i]=a[i]^b[i];
+System.out.println("Data to be transmitted: ");
+for(int i=0;i<len;i++)
 {
-int i,j,k,select[10]={0};
-printf("Topological ordering is:");
-for(i=0;i<n;i++)
-{
-cal_colsum();
-for(j=0;j<n;j++)
-{
-if(select[j]==0 && colsum[j]==0)/
-break;
+System.out.print(a[i]+" ");
 }
-printf("%d ",j);
-select[j]=1;
-for(k=0;k<n;k++)
-cost[j][k]=0;
+System.out.println();
+System.out.println("Enter the Reveived Data: ");
+for(int i=0;i<len;i++)
+{a
+[i]=sc.nextInt();
 }
-}
-void main()
-{
-printf("Enter no. of Vertices: ");
-scanf("%d",&n);
-printf("Enter the cost matrix\n");
-for(int i=0;i<n;i++)
-for(int j=0;j<n;j++)
-scanf("%d",&cost[i][j]);
-source_removal();
-}
+ob.div(a, k);
+for(int i=0;i<len;i++)
+{ if(a[i]!=0)
+{f
+lag=1; break;
+}} if(flag==1)
+System.out.println("error in data");
+else
+System.out.println("no error");
+}}
